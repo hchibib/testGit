dffc9262a1fe739febefc1d78a646b6a4079b4e6
hchibib <henrychibib@gmail.com>
2021-11-26 03:22:32 -0600

    added helloGit.py

---
+++ helloGit.py
@@ -0,0 +1,10 @@
+# By submitting this assignment, I agree to the following:
+# “Aggies do not lie, cheat, or steal, or tolerate those who do”
+# “I have not given or received any unauthorized aid on this assignment”
+#
+# Name: HENRY CHIBIB
+# Section: 563
+# Assignment: 
+# Date:
+
+print("Hello Git")
